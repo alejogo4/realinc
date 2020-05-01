c52744718f066ac437885049f13768191c7a4512
Alejandro Giraldo Duque <alejogo4>
2020-05-01 16:12:09 -0500

    Añadir efectos y contenido parallax

--- src/Components/Do/Do.js
+++ src/Components/Do/Do.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import './do.css';
 import Stars from './../Stars/Stars';
+import { ReactComponent as PlanetRight  } from './../../resources/plante_circle.svg';
+import { ReactComponent as PlanetLeft  } from './../../resources/planet_squared.svg';
+
+import { Parallax } from 'react-scroll-parallax';
 const Do = () => {
     return (
         <section className="container-full bg-stars do position-relative">
-            <Stars/>
-            <h2 class="tittle color-white font-bold">¿Y QUE HACEMOS?</h2>
+            <div>
+                <Stars/>
+                <div className="content-text">
+                    <h2 className="tittle color-white font-bold">¿QUE HACEMOS?</h2>
+                    <p className="paragraph">Cuando se trata de proyectos digitales, Tenemos el toque</p>
+                    <p className="paragraph"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quidem fuga quibusdam exercitationem. Odio sunt perspiciatis quae aliquam perferendis praesentium, minus commodi hic consequuntur dignissimos fugit voluptate quod iure illum.</p>
+                </div>
+                <Parallax className="position-absolute planet-left"  y={[-20, 40]}>
+                    <PlanetLeft className="planets"/>
+                </Parallax>
+                <Parallax className="position-absolute planet-right"  y={[-20, 5]}>
+                    <PlanetRight className="planets"/>
+                </Parallax>
+                </div>
         </section>
     )
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import './footer.css';
-import Stars from './../Stars/Stars';
-import { ReactComponent as CloudWhiteLeft  } from './../../resources/nube_izquierda.svg';
-import { ReactComponent as CloudWhiteRight  } from './../../resources/nube_derecha.svg';
-import { Parallax } from 'react-scroll-parallax';
-
+import React from "react";
+import "./footer.css";
+import Stars from "./../Stars/Stars";
+import { ReactComponent as CloudWhiteLeft } from "./../../resources/nube_izquierda.svg";
+import { ReactComponent as CloudWhiteRight } from "./../../resources/nube_derecha.svg";
+import { ReactComponent as Satellite } from "./../../resources/stelite.svg";
+import { Parallax } from "react-scroll-parallax";
 
 const Footer = () => {
-    return (
-        <footer className="container-full bg-stars footer position-relative">
-            <h2 class="tittle color-white font-bold">TRABAJEMOS JUNTOS</h2>
-            <Stars/>
-            <Parallax className="cloud cloud-white-left"  x={[20, 0]}>
-                <CloudWhiteLeft className="cloud-parallax"/>
-            </Parallax>
-            <Parallax className="cloud cloud-white-right"  x={[0, 20]}>
-                <CloudWhiteRight className="cloud-parallax"/>
-            </Parallax>
-            
-        </footer>
-    )
-}
+  return (
+    <footer className="container-full bg-stars footer position-relative">
+      <h2 class="tittle color-white font-bold">TRABAJEMOS JUNTOS</h2>
+      <div className="form-container">
+        <textarea></textarea>
 
-export default Footer
+        <button className="button-contact">Enviar</button>
+      </div>
+
+      <Stars />
+      <Parallax className="cloud cloud-white-left" x={[20, 0]}>
+        <CloudWhiteLeft className="cloud-parallax" />
+      </Parallax>
+      <Parallax className="cloud cloud-white-right" x={[0, 20]}>
+        <CloudWhiteRight className="cloud-parallax" />
+      </Parallax>
+
+      <Parallax className="satelite" y={[20,90]} x={[0, 10]}>
+        <Satellite className="cloud-parallax" />
+      </Parallax>
+    </footer>
+  );
+};
+
+export default Footer;
